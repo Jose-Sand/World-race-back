ca3e47039c6db643cbb3643386f3107442d6cf1c
Luis Marquez <lmarqueza@uninorte.edu.co>
2025-03-16 23:07:42 -0500

    fix(pdf-service): fix executable path for chromium

--- src/pdf/pdf.service.ts
+++ src/pdf/pdf.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { createPdf } from '@luism27/nestjs-html-pdf';
 import * as path from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PdfService {
 
+  constructor(private readonly configService: ConfigService) {}
   async generatePdf(options: any, data: any = {}){
     const filePath = path.join(process.cwd(), 'templates', 'donation.hbs');
     return createPdf(filePath, options, data, {
+      ...this.configService.get('NODE_ENV') === 'production' ? {
+        executablePath: '/usr/bin/google-chrome-stable',
+      }: {},
       headless: 'shell',
-      args: ['--enable-gpu'],
+      args: ['--enable-gpu', '--no-sandbox'],
     })
   }
 }
